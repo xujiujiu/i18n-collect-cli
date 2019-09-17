dbeb1e09731185e04161782c0e09f06f8d835d69
xujiujiu <906784584@qq.com>
2019-09-17 21:04:01 +0800

    Create writeLang.js

---
+++ writeLang.js
@@ -0,0 +1,141 @@
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const filePath = path.resolve('./src')
+const readline = require('readline')
+let test = /[\u4E00-\u9FA5\uF900-\uFA2D\uff0c\u3002\uff1f\uff01\u3001]{1,}(\d*\u0020?[\u4E00-\u9FA5\uF900-\uFA2D\uff0c\u3002\uff1f\uff01\u3001]{1,})?/
+let rl = null
+let isNote = false
+let dirU = os.type().toLowerCase().includes('window') ? '\\' : '/' // window环境使用‘\\’mac系统使用‘/’
+
+let lang = JSON.parse(fs.readFileSync(path.join(__dirname, '/lang.json'), 'utf-8'))
+let langObj = {}
+for (let cs_key in lang) {
+  for (let key in lang[cs_key]) {
+    langObj[lang[cs_key][key]] = cs_key + '.' + key
+  }
+}
+
+function delDir (path) {
+  let files = []
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path)
+    files.forEach((file, index) => {
+      let curPath = path + '/' + file
+      if (fs.statSync(curPath).isDirectory()) {
+        delDir(curPath)
+      } else {
+        fs.unlinkSync(curPath)
+      }
+    })
+    fs.rmdirSync(path)
+  }
+}
+function getStat (newpath) {
+  // console.log('path', newpath)
+  if (!fs.existsSync(newpath)) {
+    fs.mkdirSync(newpath, { recursive: true })
+  }
+}
+function readFileList (dir, filesList = []) {
+  // return new Promise((resolve, reject) => {
+  const files = fs.readdirSync(dir)
+  files.forEach((item, index) => {
+    let fullPath = path.join(dir, item)
+    const stat = fs.statSync(fullPath)
+    if (stat.isDirectory()) {
+      readFileList(path.join(dir, item), filesList)// 递归读取文件
+    } else {
+      let ContentLine = ''
+      let path2 = fullPath.replace(path.join(__dirname, dirU + 'src' + dirU), '')
+      if (path2.includes('pages') || path2.includes('components')) {
+        let newPath = path.join(__dirname, dirU + 'srcDist' + dirU + path2)
+        getStat(newPath.replace(dirU + path.basename(newPath), ''))
+        let extname = path.extname(fullPath)
+        if (['.vue', '.js'].includes(extname)) {
+          fs.writeFileSync(newPath, {
+            flag: 'a+'
+          })
+          isNote = false
+          let fsWrite = fs.createWriteStream(newPath)
+          rl = readline.createInterface({
+            input: fs.createReadStream(fullPath),
+            output: fsWrite
+          })
+          rl.on('line', (line) => {
+            let content = isNote ? '' : line
+            ContentLine = line
+            if (line.includes('/*')) {
+              isNote = true
+              content = line.slice(0, line.indexOf('/*'))
+            }
+            if (line.includes('*/')) {
+              if (isNote) {
+                isNote = false
+                content = line.slice(line.indexOf('*/') + 2)
+              }
+            }
+            if (line.includes('<!--')) {
+              isNote = true
+              content = line.slice(0, line.indexOf('<!--'))
+            }
+            if (line.includes('-->')) {
+              if (isNote) {
+                isNote = false
+                content = line.slice(line.indexOf('-->') + 3)
+              }
+            }
+            if (line.includes('//')) content = line.slice(0, line.indexOf('//'))
+
+            let str = content.match(test)
+            while (str) {
+              str = content.match(test)
+              if (str) {
+                str = str[0]
+                let otherStr = ''
+                if (content.indexOf('\'' + str) > -1) {
+                  let contentArr = content.split(str)
+                  otherStr = contentArr[1].slice(0, contentArr[1].indexOf('\''))
+                }
+                if (content.indexOf('"' + str) > -1) {
+                  let contentArr = content.split(str)
+                  otherStr = contentArr[1].slice(0, contentArr[1].indexOf('"'))
+                }
+                if (content.indexOf('>' + str) > -1) {
+                  let contentArr = content.split(str)
+                  otherStr = contentArr[1].slice(0, contentArr[1].indexOf('<'))
+                }
+                str += otherStr.replace(/{{(.*)}}/g, '')
+              }
+              content = content.replace(str, 'a1')
+              if (['.vue'].includes(extname)) {
+                ContentLine = ContentLine.replace(`placeholder="${str}"`, `:placeholder="$t('${langObj[str]}')"`)
+                ContentLine = ContentLine.replace(`title="${str}"`, `:title="$t('${langObj[str]}')"`)
+                ContentLine = ContentLine.replace(`label="${str}"`, `:label="$t('${langObj[str]}')"`)
+                ContentLine = ContentLine.replace(`tab="${str}"`, `:tab="$t('${langObj[str]}')"`)
+                ContentLine = ContentLine.replace(`'${str}'`, `this.$t('${langObj[str]}')`)
+                ContentLine = ContentLine.replace(`"${str}"`, `"{{$t('${langObj[str]}')}}"`)
+                ContentLine = ContentLine.replace(`${str}`, `{{$t('${langObj[str]}')}}`)
+              }
+              if (['.js'].includes(extname)) {
+                ContentLine = ContentLine.replace(`'${str}'`, `i18n.t('${langObj[str]}')`)
+                ContentLine = ContentLine.replace(`"${str}"`, `i18n.t('${langObj[str]}')`)
+              }
+              console.log(ContentLine)
+            }
+            fsWrite.write(ContentLine + os.EOL)
+          })
+          rl.on('close', () => {
+            // resolve(ContentLine)
+          })
+        }
+        filesList.push(fullPath)
+      } else {
+        return false
+      }
+    }
+  })
+}
+
+delDir(path.join(__dirname, dirU + 'srcDist'))
+readFileList(filePath)
