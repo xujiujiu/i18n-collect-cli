31f5f6d56a2f4e825f905fe5ff2b0cf042c08182
xujiujiu <906784584@qq.com>
2019-12-10 22:21:34 +0800

    更新指令

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ program.version(require('./package').version)
 
 // 中文收集并生成key对应中文的json文件
 program.command('getlang [src]')
-    .description('对[src]目录下的.vue .js 文件进行中文收集，默认src目录下面的pages和components目录')
+    .description('对[src]目录下的.vue .js 文件进行中文收集，默认src目录下面的pages和components目录\n')
     .option('-f, --filename <filename>', '[optional]设置生成的文件名，默认为 zh_cn.json，需为 .json 文件\n')
-    .option('-d, --dir <pages>', '[optional]需要收集中文的文件夹，默认为pages 和 components', value => {
+    .option('-d, --dir <dir>', '[optional]需要收集中文的文件夹，默认为pages 和 components', value => {
         return value.split(',')
     })
-    .action((src = 'src', {pages = ['pages', 'components'], filename = 'zh_cn.json'}) => {
+    .action((src = 'src', {dir = ['pages', 'components'], filename = 'zh_cn.json'}) => {
         if(filename.includes('.json')) {
-            getLang.getLang(src, pages, filename)
+            getLang.getLang(src, dir, filename)
         } else {
             console.error('filename 必须是json文件类型')
         }
@@ -29,13 +29,13 @@ program.command('getlang [src]')
 // 复制项目并将对应json中中文对应的key按照i18n的标准写入项目
 program.command('writelang [src]')
     .description('将项目需要配置国际化的文件复制一份，并将文件中的中文替换成对应的key值，src为复制的文件目录, 默认为srcDist\n')
-    .option('-f, --filename <filename>', '[optional]需要获取中文key值的文件，默认为 zh_cn.json')
-    .option('-d, --dir <pages>', '[optional]需要替换的文件夹，默认为 pages 和 components', value => {
+    .option('-f, --filename <filename>', '[optional]需要获取中文key值的文件，默认为 zh_cn.json\n')
+    .option('-d, --dir <dir>', '[optional]需要替换的文件夹，默认为 pages 和 components\n', value => {
         return value.split(',')
     })
-    .action((src = 'srcDist', {pages = ['pages', 'components'], filename = 'zh_cn.json'}) => {
+    .action((src = 'srcDist', {dir = ['pages', 'components'], filename = 'zh_cn.json'}) => {
         if(filename.includes('.json')) {
-            writeLang.writeLang(src, pages, filename)
+            writeLang.writeLang(src, dir, filename)
         } else {
             console.error('filename 必须是json文件类型')
         }
@@ -43,7 +43,7 @@ program.command('writelang [src]')
 
 // 将国际化js文件转成excel并输出，地址默认为执行脚本所在位置且不能修改    
 program.command('toexcel [url] [translateUrl] [filename]')
-    .description('将多语言js文件转成excel表格\n[url]多语言js文件路径, 如./src/lib/xx.js, \n[translateUrl] 选填，为已经翻译的语言的js，用于提取语言增量，\n[filename]生成的excel文件名，默认当前位置，不能修改存储地址，格式可以为".xls", ".xml",".xlsx",".xlsm"\n')
+    .description('将多语言js文件转成excel表格\n[url]多语言js文件路径, 如/src/lib/xx.js, \n[translateUrl] 选填，为已经翻译的语言的js，用于提取语言增量，\n[filename]生成的excel文件名，默认当前位置，不能修改存储地址，格式可以为".xls", ".xml",".xlsx",".xlsm"\n')
     // .option('-u, --url <url>', '[must]多语言js文件路径, 如./src/lib/xx.js')
     // .option('-f, --filename <filename>', '[optional]生成的excel文件名，默认当前位置，不能修改存储地址，格式可以为".xls", ".xml",".xlsx",".xlsm"')
     .action((url, translateUrl,filename = 'zh.xlsx') => {
